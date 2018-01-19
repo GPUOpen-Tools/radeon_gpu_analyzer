3328dddb9ff23d32b537c0f0d4152b8850197c27
AmitBM <amit.bm3@gmail.com>
2018-01-19 13:54:13 -0500

    Further updates for 1.4 release.
    Change-Id: I585c4fa40704718d1d2cb7f2c1503ba56dda5d1e

--- RadeonGPUAnalyzerCLI/src/kcUtils.cpp
+++ RadeonGPUAnalyzerCLI/src/kcUtils.cpp
@@ -27,12 +27,6 @@
 #include <dirent.h>
 #endif
 
-#ifndef _WIN32
-#pragma GCC diagnostic ignored "-Wunused-variable"
-#include <dirent.h>
-#endif
-
-using namespace std;
 using namespace beKA;
 
 // Static constants.
@@ -1443,118 +1437,6 @@ bool kcUtils::GenerateCliMetadataFile(const std::string& fileName, const rgFileE
     }
 
     ret = ret && (doc.SaveFile(fileName.c_str()) == tinyxml2::XML_SUCCESS);
-
-    return ret;
-}
-
-bool kcUtils::FileNotEmpty(const std::string fileName)
-{
-    bool  ret = false;
-    if (!fileName.empty())
-    {
-        std::ifstream file(fileName);
-        ret = (file.good() && file.peek() != std::ifstream::traits_type::eof());
-    }
-    return ret;
-}
-
-bool kcUtils::ReadProgramSource(const string& inputFile, string& programSource)
-{
-    std::ifstream input;
-
-#ifndef _WIN32
-    // test if the input file is a directory.
-    // On some Linux machine ifstream open will be valid if it is a directory
-    // but will not allow to read data which will cause a crash when trying to read the data
-    DIR* pDir;
-
-    pDir = opendir(inputFile.c_str());
-
-    if (pDir != NULL)
-    {
-        (void)closedir(pDir);
-        return false;
-    }
-
-#endif
-
-    // Open (at e)nd of file.  We want the length below.
-    // Open binary because it's faster & other bits of code deal OK with CRLF, LF etc.
-    input.open(inputFile.c_str(), ios::ate | ios::binary);
-
-    if (!input)
-    {
-        return false;
-    }
-
-    std::ifstream::pos_type fileSize = 0;
-
-    fileSize = input.tellg();
-
-    if (fileSize == static_cast<std::ifstream::pos_type>(0))
-    {
-        input.close();
-        return false;
-    }
-
-    input.seekg(0, ios::beg);
-
-    programSource.resize(size_t(fileSize));
-    input.read(&programSource[0], fileSize);
-
-    input.close();
-    return true;
-}
-
-
-bool kcUtils::WriteBinaryFile(const std::string& fileName, const std::vector<char>& content, LoggingCallBackFunc_t pCallback)
-{
-    bool ret = false;
-    ofstream output;
-    output.open(fileName.c_str(), ios::binary);
-
-    if (output.is_open() && !content.empty())
-    {
-        output.write(&content[0], content.size());
-        output.close();
-        ret = true;
-    }
-    else
-    {
-        std::stringstream log;
-        log << "Error: Unable to open " << fileName << " for write.\n";
-
-        if (pCallback != nullptr)
-        {
-            pCallback(log.str());
-        }
-    }
-
-    return ret;
-}
-
-bool kcUtils::WriteTextFile(const std::string& fileName, const std::string& content, LoggingCallBackFunc_t pCallback)
-{
-    bool ret = false;
-    ofstream output;
-    output.open(fileName.c_str());
-
-    if (output.is_open())
-    {
-        output << content << std::endl;
-        output.close();
-        ret = true;
-    }
-    else
-    {
-        std::stringstream log;
-        log << "Error: Unable to open " << fileName << " for write.\n";
-
-        if (pCallback != nullptr)
-        {
-            pCallback(log.str());
-        }
-    }
 
     return ret;
 }
